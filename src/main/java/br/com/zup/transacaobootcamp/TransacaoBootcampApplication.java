51c839d12decebf5e0fa50c0726609e8dd957e4b
montivaljuniorzup <montival.junior@zup.com.br>
2020-12-11 14:48:17 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/zup/transacaobootcamp/TransacaoBootcampApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.transacaobootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoBootcampApplication.class, args);
+	}
+
+}
